4bcd39db463f4a857eee0abbd227b33fa872291e
Saumitra Bagchi <saumitra.bagchi@ericsson.com>
2021-04-05 15:42:29 +0300

    Navbar srat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <Router>
+    
+        <Navbar/>
+        <Route path='/' exact/>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                     TRVL
+                    </Link>
+
+                </div>
+
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
