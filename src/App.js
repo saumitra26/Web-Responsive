3ea60ef285198fc3ada7f731e4d2245878bc2421
Saumitra Bagchi <saumitra.bagchi@ericsson.com>
2021-04-06 12:44:53 +0300

    Add video to home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <Router>
     
         <Navbar/>
-        <Route path='/' exact/>
+        <Route path='/' exact component={Home}/>
     </Router>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import '../../App.css'
+import HeroSection from '../HeroSection'
+const Home = () => {
+    return (
+        <>
+            <HeroSection/>
+        </>
+    )
+}
+
+export default Home
